7bd08f74f487c1070cd32e4ff39e39df686a35f2
marcusng8 <64470453+marcusng8@users.noreply.github.com>
2020-06-01 08:29:09 -0300

    Create dbVideos.js

---
+++ js/dbVideos.js
@@ -0,0 +1,45 @@
+var db = {
+    videos:[
+        {
+            nome:'Monitoria Calculo I',
+            link:'https://www.youtube.com/embed/rdcuOytxjKY',
+            data:[
+                {
+                    dia:09,
+                    mes:03,
+                    ano:2020
+                }
+            ]
+        },{
+            nome:'Monitoria AED I',
+            link:'https://www.youtube.com/embed/rdcuOytxjKY',
+            data:[
+                {
+                    dia:14,
+                    mes:02,
+                    ano:2020
+                }
+            ]
+        },{
+            nome:'Monitoria Introdução a computação',
+            link:'https://www.youtube.com/embed/rdcuOytxjKY',
+            data:[
+                {
+                    dia:03,
+                    mes:03,
+                    ano:2020
+                }
+            ]
+        },{
+            nome:'Monitoria Religião',
+            link:'https://www.youtube.com/embed/rdcuOytxjKY',
+            data:[
+                {
+                    dia:09,
+                    mes:04,
+                    ano:2020
+                }
+            ]
+        }
+    ]
+}
